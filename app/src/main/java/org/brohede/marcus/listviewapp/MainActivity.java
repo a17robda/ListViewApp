5bd1524e805e6434cfaa4ef0b5d1cee28f8c86f9
a17robda <a17robda@student.his.se>
2018-04-09 23:34:26 +0200

    0.1 Add listview + toasts for every array item.

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -1,9 +1,16 @@
 package org.brohede.marcus.listviewapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // The onCreate method is run when the app is created.
         // Before you can implement this you need to create the layout xml files that
         // will hold/show your data created here. You need three create things:
@@ -28,6 +36,23 @@ public class MainActivity extends AppCompatActivity {
         // * my_item_textview - This is the ID to the actual TextView that will contain the text for
         //                      an individual item in the ListView we are creating.
         // Here you should enter your code that fills the ListView
+        List<String> listData = new ArrayList<String>(Arrays.asList(mountainNames));
+
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.list_item_textview,R.id.my_item_textview, listData);
+
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Toast.makeText(MainActivity.this, "Mountain name: " + mountainNames[i].toString() + "\n" + "Location: " + mountainLocations[i] + "\n" + "Height: " + mountainHeights[i] + "m", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
         // 1. Create an array
         // 2. Create a List object with your array from step 1 as in-data
         // 3. Create an ArrayAdapter object that connects
